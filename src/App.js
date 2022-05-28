72a2962e8f681a1d0157fa0fdc8eb67937b8b32a
Chetan Kumar Soni <sonichetan909@gmail.com>
2022-05-28 12:00:27 +0530

    Digital Library files added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Section from './Components/Section';
+import Aside from './Components/Aside';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Section/>
+    <Aside/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
