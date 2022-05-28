856d2b92af9e6ed36dd8c222dcf9cf17cf62865b
Chetan Kumar Soni <sonichetan909@gmail.com>
2022-05-28 14:06:32 +0530

    Navbar added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import '../App.css';
 
-export default function navbar() {
+export default function Navbar() {
+  const show_hide_Nav= (e)=>{
+    e.preventDefault();
+    let nav=document.getElementById('nav');
+    if(nav.style.display!=='none')
+    {
+      nav.style.display='none';
+    }
+    else
+    {
+      nav.style.display='inline';
+    }
+  };
   return (
-    <h1>Hello</h1>
+    <nav className="navbar">
+      <img src={require('../Images/menu_white.png')} alt="nav_icon" onClick={show_hide_Nav}/>
+      <center>
+       <ul id='nav'>
+         <li className="item">Home</li>
+         <li className="item">Category</li>
+         <li className="item">About Us</li>
+         <li className="item">Sign Up</li>
+         <li className="item">Log In</li>
+       </ul>
+       </center>
+    </nav>
   )
 }
